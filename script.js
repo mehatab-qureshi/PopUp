48c4328b45c298b426c9f6af15c12fee10de4d97
Mehatab Qureshi <mahetabqureshi5676@gmail.com>
2026-02-03 22:56:20 +0530

    functionality update

---
+++ script.js
@@ -0,0 +1,9 @@
+let popup = document.querySelector(".popUp");
+
+let openPop = () => {
+  popup.classList.add("popup_open");
+};
+
+let closePop = () => {
+  popup.classList.remove("popup_open");
+};
